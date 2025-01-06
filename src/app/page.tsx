2c782583acee07f33624c0886ad6bf85cd8514aa
Hazhir2002 <HazhirYousefi2002@gmail.com>
2025-01-06 12:19:19 +0330

    added websocket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+
 import Spline from "@splinetool/react-spline/next";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8000/ws/data/");
+
+    socket.onmessage = (event) => {
+      const receivedData = JSON.parse(event.data);
+      setData((prevData) => [...prevData, receivedData.message]); // Append new data
+    };
+
+    socket.onopen = () => {
+      console.log("WebSocket connection opened.");
+    };
+
+    socket.onclose = () => {
+      console.log("WebSocket connection closed.");
+    };
+
+    return () => {
+      socket.close(); // Cleanup on component unmount
+    };
+  }, []);
+
   return (
     <div>
       <Suspense>
